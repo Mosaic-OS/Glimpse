4824aaeede6ae8d7bcfa431f38d4a0a0f8335dcc
SuperNeatJaguar <293907574+SuperNeatJaguar@users.noreply.github.com>
2026-07-27 16:58:29 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "org.lineageos.glimpse"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "org.lineageos.glimpse"
         minSdk = 30
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
     }
